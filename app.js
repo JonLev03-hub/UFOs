e5fe4c67f1ed2d090a0576d5f00bde48462511de
Jonathan Leverenz <jonlev03@gmail.com>
2021-12-05 16:23:47 -0500

    added items to the webpage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const tableData = data;
 
 // Reference the HTML table using d3
-var tbody = d3.select("tbody");
+var tbody = d3.se lect("tbody");
 
 function buildTable(data) {
     // First, clear out any existing data
@@ -23,3 +23,21 @@ function buildTable(data) {
       );
     });
   }
+  function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
+     // Rebuild the table using the filtered data
+    buildTable(filteredData);
+  }
+  // Attach an event to listen for the form button
+  d3.selectAll("#filter-btn").on("click", handleClick);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
